34f998ff4bc57e9cfa9cf507c2d8cd8b1981ac91
Yorman Rojas <yormanxrojas@gmail.com>
2024-02-09 10:08:48 -0600

    init project

---
+++ src/test/java/com/backend/taskplanner/TaskplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.taskplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
